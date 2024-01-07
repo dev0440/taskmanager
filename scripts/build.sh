22e46f4c4d8a9988f9a8dd7e821726f3d61577b2
Andrii Stiahar <a.stiagar@apiko.com>
2024-01-07 15:04:43 +0200

    Refactor build scripy

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,14 @@
 echo $DOCKERHUB_ACCESS_TOKEN | docker login -u andriihub --password-stdin
 
 if [ $? != 0 ]; then
-	echo 'Login failed'
 	exit 1
 fi
+
+imagename='andriihub/taskmanager:latest'
 
 docker build --tag andriihub/taskmanager:latest .
 
 if [ $? != 0 ]; then
-	echo 'Build failed'
 	exit 1
 fi
 
