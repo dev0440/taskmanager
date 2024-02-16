868249f63f81062ad7d8ba97fae0df529ba6e364
Andrii Stiahar <a.stiagar@apiko.com>
2024-02-16 13:55:28 +0200

    Remove obsolete code

--- src/application/routes/auth/signup.ts
+++ src/application/routes/auth/signup.ts
@@ -1,39 +1,5 @@
 import { FastifyInstance, FastifyRequest, RouteOptions } from 'fastify';
 
-interface SingupCredentials {
-  email: string;
-  password: string;
-}
-
-export interface User {
-  email: string;
-  password: string;
-}
-
-interface AuthService {
-  hashPassword: (password: string) => string;
-}
-
-interface UserService {
-  saveUser: (user: User) => Promise<User>;
-}
-
-export class SignupController {
-  constructor(
-    private userRepository: UserService,
-    private authService: AuthService,
-  ) {}
-
-  async signup(credentials: SingupCredentials): Promise<User> {
-    const password = this.authService.hashPassword(credentials.password);
-
-    const user = { email: credentials.email, password };
-    await this.userRepository.saveUser({ email: credentials.email, password });
-
-    return user;
-  }
-}
-
 interface ISignup {
   email: string;
   password: string;
