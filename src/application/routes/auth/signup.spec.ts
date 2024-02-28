8e1b04097367dcd58ba8506d109b29954d1860ae
Andrii Stiahar <a.stiagar@apiko.com>
2024-02-28 15:42:05 +0200

    Refactor router test

--- src/application/routes/auth/signup.spec.ts
+++ src/application/routes/auth/signup.spec.ts
@@ -5,31 +5,38 @@ import { FastifyInstance } from 'fastify';
 import { Left, Right } from '../../../core/common/Either';
 import { signupRoutes } from './signup';
 import { AuthFailures } from '../../../core/modules/user/auth/failures';
-import { HTTP_ERRORS } from '../../common/errors';
+import { HttpErrorFormatter } from '../../common/errors';
 
 const token = faker.string.sample();
 const password = faker.internet.password();
 const email = faker.internet.email();
 
+const mockError = {
+  code: 404,
+  message: 'Http error',
+};
+
 const signupImpl = jest
   .fn()
   .mockImplementation(() => Promise.resolve(Right.of(token)));
 
+const mockErrorFormatter = jest.fn().mockImplementation(() => mockError);
+
 describe('Signup routes', () => {
   let server: FastifyInstance;
+  jest.spyOn(SignupUseCase.prototype, 'execute').mockImplementation(signupImpl);
+
+  jest
+    .spyOn(HttpErrorFormatter.prototype, 'of')
+    .mockImplementation(mockErrorFormatter);
 
   beforeEach(() => {
-    jest
-      .spyOn(SignupUseCase.prototype, 'execute')
-      .mockImplementation(signupImpl);
-  });
-
-  beforeAll(() => {
     server = new App([], [signupRoutes], {}).getServer();
   });
 
   afterEach(() => {
-    jest.resetAllMocks();
+    signupImpl.mockClear();
+    mockErrorFormatter.mockClear();
   });
 
   it('should signup user', async () => {
@@ -64,9 +71,12 @@ describe('Signup routes', () => {
       body,
     });
 
-    const expectedResponse = HTTP_ERRORS[AuthFailures.UserAlreadyExistsFailure];
+    expect(mockErrorFormatter).toHaveBeenCalledTimes(1);
+    expect(mockErrorFormatter).toHaveBeenCalledWith({
+      type: AuthFailures.UserAlreadyExistsFailure,
+    });
 
-    expect(res.statusCode).toEqual(expectedResponse.code);
-    expect(res.json()).toEqual({ message: expectedResponse.message });
+    expect(res.statusCode).toEqual(mockError.code);
+    expect(res.json()).toEqual({ message: mockError.message });
   });
 });
